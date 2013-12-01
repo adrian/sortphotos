955f8899ef214e3e23e7ab7c8a412a095860ac75
Adrian Smith <adrian@17od.com>
2013-12-01 19:47:16 +0000

    Add regex support to file rename

--- src/rename.py
+++ src/rename.py
@@ -3,23 +3,37 @@
 
 import glob
 import argparse
-import shutil
 import os
+import re
 
-def rename(src_dir, dest_dir, file_pattern):
+def rename(src_dir, dest_dir, file_pattern, search_pattern, replace):
     """Rename all files in the give directory by removing the first 10
     characters"""
     for src_file in glob.iglob(os.path.join(src_dir, file_pattern)):
         filename = os.path.basename(src_file)
-        shutil.copy2(src_file, os.path.join(dest_dir, filename[11:]))
+        newfilename = re.sub(search_pattern, replace, filename)
+        os.rename(src_file, os.path.join(dest_dir, newfilename))
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
-                                     description='Rename files in a given directory by \nremoving the first 10 characters from their name')
-    parser.add_argument('src_dir', type=str, help='source directory')
-    parser.add_argument('dest_dir', type=str, help='destination directory')
-    parser.add_argument('file_pattern', type=str, help='filename search pattern')
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description="Rename selected files in a directory using a regex to perform a\n"
+                    "search/replace on the filename.\n\nFor example, to rename all JPG "
+                    "files in /tmp/s, placing them into\n/tmp/d and removing the string "
+                    "'iphone _' from the start of the filename:\n\n"
+                    "    ./rename.py /tmp/s /tmp/d *.JPG '^iphone _' ''")
+    parser.add_argument('src_dir', type=str,
+        help='source directory')
+    parser.add_argument('dest_dir', type=str,
+        help='destination directory')
+    parser.add_argument('file_pattern', type=str,
+        help='filename search pattern')
+    parser.add_argument('search_pattern', type=str,
+        help='pattern to search for in filename')
+    parser.add_argument('replace', type=str,
+        help='string to replace search_pattern with')
 
     args = parser.parse_args()
 
-    rename(args.src_dir, args.dest_dir, args.file_pattern)
+    rename(args.src_dir, args.dest_dir, args.file_pattern,
+        args.search_pattern, args.replace)
